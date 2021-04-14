ac1b54be588deecbb2e8324fa44b3a9cb81a0677
afollette <follette.alex@gmail.com>
2021-04-14 11:23:14 +0100

    Add tests to FileReaderTests. Modified FileReader
    Because:
    -- Tests are for recent additions to FileReader
    -- readTsvOrReturnEmpty was not behaving as described
    
    * add check to readTsvOrReturnEmpty to check for non-existent file
    
    Reference:#579

--- src/main/java/org/pdxfinder/validator/tableutilities/FileReader.java
+++ src/main/java/org/pdxfinder/validator/tableutilities/FileReader.java
@@ -27,6 +27,7 @@ public class FileReader {
   private static final Logger log = LoggerFactory.getLogger(FileReader.class);
 
   private FileReader() {
+    throw new IllegalStateException("Utility classs");
   }
 
   public static List<Table> readXlsx(InputStream inputStream) throws IOException {
@@ -97,14 +98,16 @@ public class FileReader {
     log.trace("Reading tsv file {}", file);
     System.out.print(String.format("Reading tsv file %s\r", file));
     try {
-      dataTable = readTsv(file);
+      if (file.exists()) {
+        dataTable = readTsv(file);
+      }
     } catch (IOException e) {
       log.error("There was an error reading the tsv file", e);
     }
     return dataTable;
   }
 
-  public static Table readTsv(File file) throws IOException {
+  private static Table readTsv(File file) throws IOException {
     CsvReadOptions.Builder builder = CsvReadOptions
         .builder(file)
         .sample(false)
@@ -112,5 +115,4 @@ public class FileReader {
     CsvReadOptions options = builder.build();
     return Table.read().usingOptions(options);
   }
-
 }
